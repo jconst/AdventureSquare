971f027ffa53b389bb41e48b69c63180bb1d2bcc
Joseph Constantakis <jcon5294@gmail.com>
2014-10-12 23:14:41 -0400

    add unity gitignore, get basic movement & gravity system working

---
+++ Assets/Scripts/KeyConfig.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class KeyConfig 
+{
+    public static Dictionary<KeyCode, Vector2> HeadingForKey = 
+              new Dictionary<KeyCode, Vector2> {
+        {KeyCode.UpArrow,    new Vector2(0, 1)},
+        {KeyCode.DownArrow,  new Vector2(0, -1)},
+        {KeyCode.LeftArrow,  new Vector2(-1, 0)},
+        {KeyCode.RightArrow, new Vector2(1, 0)}
+    };
+
+    public static Dictionary<KeyCode, Vector2> GravityForKey = 
+              new Dictionary<KeyCode, Vector2> {
+        {KeyCode.W, new Vector2(0, 1)},
+        {KeyCode.S, new Vector2(0, -1)},
+        {KeyCode.A, new Vector2(-1, 0)},
+        {KeyCode.D, new Vector2(1, 0)}
+    };
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Player : MonoBehaviour
+{
+    const float FUPDATE_HZ = 50f;
+    public float speed;
+
+    const float GRAVITY_SCALE = 10f;
+    public Vector2 gravity {
+        get {
+            return Physics2D.gravity.normalized;
+        }
+        set {
+            Physics2D.gravity = value * GRAVITY_SCALE;
+        }
+    }
+
+    void Start() {
+        Physics2D.gravity = -Vector2.up * GRAVITY_SCALE;
+    }
+    
+    void FixedUpdate() {
+        UpdateGravity();
+        Move();
+    }
+
+    void UpdateGravity() {
+        KeyConfig.GravityForKey.Where(kvp => Input.GetKeyDown(kvp.Key))
+                               .Select(kvp => kvp.Value)
+                               .ToList()
+                               .ForEach(vec => gravity = vec);
+    }
+
+    void Move() {
+
+        Vector3 heading = KeyConfig.HeadingForKey.Where(kvp => Input.GetKey(kvp.Key))
+                                                 .Select(kvp => kvp.Value)
+                                                 .Where(v => v.Abs() != gravity.Abs())
+                                                 .Aggregate(default(Vector2), (v1, v2) => v1 + v2);
+        transform.position += heading * (speed / FUPDATE_HZ);
+    }
+}
