71f9d6b1babb7ae0ff9d43766b7ac984f1e51b00
Joseph Constantakis <jcon5294@gmail.com>
2014-10-14 18:23:41 -0400

    lock gravity & movement while in air

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,12 +2,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System;
 
 public class Player : MonoBehaviour
 {
     const float speed = 12f;
-
     const float GRAVITY_SCALE = 80f;
+
+    private HashSet<Collision2D> collisions =
+        new HashSet<Collision2D> ();
     public Vector2 gravity {
         get {
             return Physics2D.gravity.normalized;
@@ -22,8 +25,10 @@ public class Player : MonoBehaviour
     }
     
     void Update() {
-        UpdateGravity();
-        Move();
+        if (TouchingFloor()) {
+            UpdateGravity();
+            Move();
+        }
     }
 
     void UpdateGravity() {
@@ -34,13 +39,31 @@ public class Player : MonoBehaviour
     }
 
     void Move() {
-
         Vector3 heading = KeyConfig.HeadingForKey.Where(kvp => Input.GetKey(kvp.Key))
                                                  .Select(kvp => kvp.Value)
                                                  .Where(v => v.Abs() != gravity.Abs())
                                                  .Aggregate(default(Vector2), (v1, v2) => v1 + v2);
         Vector3 newPos = transform.position + heading * (Time.deltaTime * speed);
         transform.position = newPos;
+    }
+
+    void OnCollisionEnter2D(Collision2D coll) {
+        if (coll.rigidbody != null) {
+            collisions.Add(coll);
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D coll) {
+        if (coll.rigidbody != null) {
+            collisions.RemoveWhere(c => c.gameObject == coll.gameObject);
+        }
+        Debug.Log(collisions.Count);
+    }
+
+    bool TouchingFloor() {
+        return collisions.SelectMany(coll => coll.contacts)
+                         .Select(contact => contact.normal)
+                         .Any(normal => Vector2.Dot(normal, gravity) < -0.9);
     }
 
     public void Die() {
